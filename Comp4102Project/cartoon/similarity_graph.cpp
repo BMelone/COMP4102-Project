edce825c88de986f5532eed722582f7bf75ea90b
Eimhin McDonald <Eimh@users.noreply.github.com>
2020-04-11 04:40:35 -0400

    clear similarity edges from image border

--- Comp4102Project/cartoon/similarity_graph.cpp
+++ Comp4102Project/cartoon/similarity_graph.cpp
@@ -2,7 +2,15 @@
 
 SimilarityGraph::SimilarityGraph(const cv::Mat &image, SimilarityGraph::SimilarityConstraint constraint) {
 	edges = cv::Mat::zeros(image.rows, image.cols, CV_8U);
-	//edges pointing out of the image are kept
+	//edges pointing out of the image are removed
+	for (int y = 0; y < image.rows; y++) {
+		*edges.ptr<uchar>(y, 0) |= 0b00101001;
+		*edges.ptr<uchar>(y, image.cols - 1) |= 0b10010100;
+	}
+	for (int x = 0; x < image.cols; x++) {
+		*edges.ptr<uchar>(0, x) |= 0b00000111;
+		*edges.ptr<uchar>(image.rows - 1, x) |= 0b11100000;
+	}
 	for (int y = 0; y < image.rows; y++) {
 		for (int x = 0; x < image.cols; x++) {
 			for (int i = -1; i <= 1; i++) {
@@ -30,9 +38,9 @@ SimilarityGraph::SimilarityGraph(const cv::Mat &image, SimilarityGraph::Similari
 								}
 							}							
 						}
-						else {
-							*edges.ptr<uchar>(y, x) |= NeighbourAsByte(i, j);
-						}
+					}
+					else {
+						*edges.ptr<uchar>(y, x) |= NeighbourAsByte(i, j);
 					}
 				}
 			}
@@ -57,22 +65,22 @@ uchar SimilarityGraph::NeighbourAsByte(int y, int x) {
 	switch (y) {
 	case -1:
 		switch (x) {
-		case -1: return 1;
-		case 0: return 2;
-		case 1: return 4;
+		case -1: return 0b00000001;
+		case 0:  return 0b00000010;
+		case 1:  return 0b00000100;
 		default: return 0;
 		}
 	case 0:
 		switch (x) {
-		case -1: return 8;
-		case 1: return 16;
+		case -1: return 0b00001000;
+		case 1:  return 0b00010000;
 		default: return 0;
 		}
 	case 1:
 		switch (x) {
-		case -1: return 32;
-		case 0: return 64;
-		case 1: return 128;
+		case -1: return 0b00100000;
+		case 0:  return 0b01000000;
+		case 1:  return 0b10000000;
 		default: return 0;
 		}
 	default: return 0;
