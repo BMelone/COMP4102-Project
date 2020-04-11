e0f6767ff1872599faa1ed0eda82a783405acecd
Benjamin Melone <melone22@rogers.com>
2020-04-10 22:46:14 -0400

    Add segment selection and improve input param configuration

--- Comp4102Project/Main.cpp
+++ Comp4102Project/Main.cpp
@@ -1,24 +1,29 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include "segment-image.h"
+#include "disjoint-set.h"
+#include <unordered_set>
 
 int threshold_val, min_size_val, sigma_val;
-const int threshold_max = 50;
-const int min_size_max = 500;
+const int threshold_max = 20;
+const int min_size_max = 200;
 const int sigma_max = 15;
 cv::Mat img;
 int num_ccs;
 float sigma, threshold;
 int min_size;
 
+universe* u;
+
 std::vector<int> xVector, yVector;
+std::unordered_set<int> savedSegments;
 
 
 void segment_and_display() {
 	sigma = sigma_val / 10;
-	threshold = threshold_val * 100;
+	threshold = threshold_val * 50;
 	min_size = min_size_val * 100;
-	cv::Mat output = segment_image(img, sigma, threshold, min_size, &num_ccs);
+	cv::Mat output = segment_image(img, sigma, threshold, min_size, &num_ccs, u);
 	printf("got %d components with k=%f and min_size=%d\n", num_ccs, threshold, min_size);
 	cv::imshow("image", output);
 	cv::imwrite("output.png", output);
@@ -45,17 +50,18 @@ void select_location(int event, int x, int y, int flags, void* param) {
 		std::cout << "x= " << x << " y= " << y << std::endl;
 		xVector.push_back(x);
 		yVector.push_back(y);
+		savedSegments.insert(u->find(y * img.cols + x)); //save the selected segment
 	}
 }
 
 int main()
 {
-	img = cv::imread("spiderman.png");
+	img = cv::imread("lenna.png");
 
-	threshold_val = 3;
-	min_size_val = 200;
+	threshold_val = 6;
+	min_size_val = 5;
 	sigma_val = 5;
-	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
 
 	cv::createTrackbar("Sigma", "image", &sigma_val, sigma_max, sigma_and_display);
 	cv::createTrackbar("Threshold", "image", &threshold_val, threshold_max, threshold_and_display);
@@ -65,5 +71,26 @@ int main()
 	segment_and_display();
 
 	cv::waitKey(0);
+
+	cv::Vec3b black;
+	black[0] = 0;
+	black[1] = 0;
+	black[2] = 0;
+
+	int width = img.cols;
+	int height = img.rows;
+	cv::Mat output = cv::Mat::zeros(cv::Size(width, height), img.type());
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			int comp = u->find(y * width + x);
+			output.at<cv::Vec3b>(y, x) = savedSegments.find(comp) != savedSegments.end() ? 
+				img.at<cv::Vec3b>(y, x) : black;
+		}
+	}
+
+	cv::imshow("image", output);
+	cv::imwrite("output2.png", output);
+	cv::waitKey(0);
+
 	return 0;
 }
